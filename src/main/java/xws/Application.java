f14d1d25ba27df0f1e42bdeb8116a23482c0f57b
xiangwenshan <id512@126.com>
2018-10-21 08:49:58 +0800

    添加注释

--- src/main/java/xws/Application.java
+++ src/main/java/xws/Application.java
@@ -5,12 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
+//通过下面的注解启动一个服务注册中心
 @EnableEurekaServer
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-//        new SpringApplicationBuilder(Application.class).web(true).run(args);
     }
 }
